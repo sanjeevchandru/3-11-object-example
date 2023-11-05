aabd58b3810406ca02a2ba98a014cb01163ab998
sanjeevchandru <chandrusanjeev5@gmail.com>
2023-11-05 14:31:05 +0530

    3-11-object-example

---
+++ main.js
@@ -0,0 +1,22 @@
+
+document.write("Object 5 Example"+"<br>");
+document.write("-----------------------"+"<br>");
+document.write("object-1 =>Bike"+"<br>");
+var arr={name:"fz-s",color:"green",fuelCapacity:"10L",priceInLakhs:1.2,warrenty:3};
+document.write(JSON.stringify(arr)+"<br><br>");
+
+document.write("object-2 =>Mark"+"<br>");
+var arr={tamil:70,english:80,maths:78,science:85,social:79};
+document.write(JSON.stringify(arr)+"<br><br>");
+
+document.write("object-3 =>person"+"<br>");
+var arr={name:"sanjeev",weight:"51 kg",height:"173 cm",color:"fair"};
+document.write(JSON.stringify(arr)+"<br><br>");
+
+document.write("object-4 =>T-shirt"+"<br>");
+var arr={category:"dress",gender:"male",price:300,fabric:"cotton",size:"L",color:"black"};
+document.write(JSON.stringify(arr)+"<br><br>");
+
+document.write("object-2 =>dhosa"+"<br>");
+var arr={shape:"round",color:"white",quantity:10,category:"food",price:15};
+document.write(JSON.stringify(arr)+"<br>");
